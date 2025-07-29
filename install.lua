f4eb7a6bb01980493ab47f1c65d9cf92af3c5cd4
stuin <stuin01@gmail.com>
2025-07-28 20:21:41 -0700

    Install script

---
+++ install.lua
@@ -0,0 +1,14 @@
+local files = {
+	"xevious.lua",
+	"tilemap1.txt"
+}
+
+local git = "https://raw.githubusercontent.com/stuin/Xevious/refs/heads/main/"
+
+--Install files
+for i = 1,#files do
+	fs.delete(shell.resolve(files[i]))
+	shell.run("wget "..git..files[i])
+end
+
+fs.move(shell.resolve("tilemap1.txt"), shell.resolve("xevious-map.txt"))
